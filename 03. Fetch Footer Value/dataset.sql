d4b217f541dbb3835bf43d237d62777b71f22833
wooomr2 <wooomr2@gmail.com>
2024-03-18 11:25:06 +0900

    Day 7

--- 03. Fetch Footer Value/dataset.sql
+++ 03. Fetch Footer Value/dataset.sql
@@ -1,3 +1,5 @@
+-- LAST NON NULL VALUE FROM EACH FIELD
+
 /*
 Write a sql query to return the footer values from input table,
 meaning all the last non null values from each field as shown in expected output.
